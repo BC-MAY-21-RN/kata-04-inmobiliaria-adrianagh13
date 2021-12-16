7870a0049c0a8fa312906058cf02ffd8ee4d1ba6
adrianagh13 <gomezhinojosa.adriana@gmail.com>
2021-12-15 22:44:28 -0600

    Initial structure created

---
+++ src/components/Card/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text } from 'react-native';
+
+export const Card = (props) => {
+    return (
+        <>
+            <Text> Card </Text>
+        </>
+    );
+};
---
+++ src/screens/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text } from "react-native";
+import { Card } from '../components'
+
+export const Home = () => {
+    
+    
+    return(
+        <>
+            <Text>Home</Text>
+            <Card />
+        </>
+    );
+};
